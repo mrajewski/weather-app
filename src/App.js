c39465083586b03b1dfb448a37856d74b224fb66
mrajewski <rajewski.ma@gmail.com>
2019-09-12 16:31:36 +0200

    background and searchBar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import './styles.scss'
 
-const apiKey='e087d0a804187eb8ee6d5386f3c90258';
+const apiKey = 'e087d0a804187eb8ee6d5386f3c90258';
 
-class App extends Component{
-  state= {
-    
-  };
+class App extends Component {
+    state = {
+        searchVal: ''
+    };
 
-  componentDidMount() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}`,{
-      method: "GET"
-    })
-        .then(resp=>resp.json())
-        .then(data=>{
-          console.log(data);
+    componentDidMount() {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}`, {
+            method: "GET"
         })
-        .catch(error=>console.log(error))
-  }
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => console.log(error))
+    }
 
-  render() {
-    return (
-        <h1>Weather App</h1>
-    )
+    handleOnChange = (e) => {
+        this.setState({
+            searchVal: e.target.value
+        })
+    };
+
+    render() {
+        return (
+            <>
+                <form action="">
+                    <input onChange={this.handleOnChange} type="text" value={this.state.searchVal}/>
+                    <button>Search</button>
+                </form>
+            </>
+        )
 
 
-  }
+    }
 
 }
 
