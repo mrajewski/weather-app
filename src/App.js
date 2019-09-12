f69c202a36027f3d86170c7a89320cb0430a8c9f
mrajewski <rajewski.ma@gmail.com>
2019-09-12 15:56:35 +0200

    fetch test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import "./styles.scss";
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test Test2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey='e087d0a804187eb8ee6d5386f3c90258';
+
+class App extends Component{
+  state= {
+    
+  };
+
+  componentDidMount() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}`,{
+      method: "GET"
+    })
+        .then(resp=>resp.json())
+        .then(data=>{
+          console.log(data);
+        })
+        .catch(error=>console.log(error))
+  }
+
+  render() {
+    return (
+        <h1>Weather App</h1>
+    )
+
+
+  }
+
 }
 
 export default App;
